a20c14e7ae1effbf06a80fb6615c4dffd16e4dfd
Dan Sivewright <dan@wixels.com>
2023-10-31 21:07:40 +0200

    Initial Commit

---
+++ umlungu/umlunguApp.swift
@@ -0,0 +1,17 @@
+//
+//  umlunguApp.swift
+//  umlungu
+//
+//  Created by Dan Sivewright on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct umlunguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
